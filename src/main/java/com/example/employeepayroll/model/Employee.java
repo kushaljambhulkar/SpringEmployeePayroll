cd4db968217058d2bd073e000a1077860ca25ea4
Kushal Jambhulkar <kushaljambhulkar82@gmail.com>
2022-11-11 21:04:32 +0530

    update: Controller to demonstrate the various

--- src/main/java/com/example/employeepayroll/model/Employee.java
+++ src/main/java/com/example/employeepayroll/model/Employee.java
@@ -1,2 +1,101 @@
-package com.example.employeepayroll.model;public class Employee {
+package com.example.employeepayroll.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue
+    private int id;
+    public String firstName;
+    private String lastName;
+    private String profilePic;
+    private String note;
+    private long salary;
+    private String startDate;
+
+    public Employee(int id, String firstName, String lastName, String profilePic, String note, long salary, String startDate) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.profilePic = profilePic;
+        this.note = note;
+        this.salary = salary;
+        this.startDate = startDate;
+    }
+
+    public Employee() {
+
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setProfilePic(String profilePic) {
+        this.profilePic = profilePic;
+    }
+
+    public String getProfilePic() {
+        return profilePic;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setSalary(long salary) {
+        this.salary = salary;
+    }
+
+    public long getSalary() {
+        return salary;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", profilePic='" + profilePic + '\'' +
+                ", note='" + note + '\'' +
+                ", salary=" + salary +
+                ", startDate=" + startDate +
+                '}';
+    }
 }
